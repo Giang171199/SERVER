2d24f4aabac59ab3b75dedf6046d3cdee343c598
Giang171199 <63645756+Giang171199@users.noreply.github.com>
2020-05-31 22:28:48 +0700

    UPDATE 31.5

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 class TCPServer
 {
-
-    const int MAX_CONNECTION = 10;
     const int PORT_NUMBER = 9999;
 
     static TcpListener listener;
+    static List<EndPoint> ConnectedClients = new List<EndPoint>();
 
     static Dictionary<string, string> _data =
         new Dictionary<string, string>
@@ -26,59 +26,128 @@ class TCPServer
         {"7","Seven"},
         {"8","Eight"},
         {"9","Nine"},
+        {"10","Ten"},
     };
 
-    public static void Main()
+    public static void Main(string []args)
     {
+        int MAX_CONNECTION = Convert.ToInt32(args[0]);
         IPAddress address = IPAddress.Parse("127.0.0.1");
 
         listener = new TcpListener(address, PORT_NUMBER);
-        Console.WriteLine("Doi ket noi ...");
+        Console.WriteLine("IP:Port of Server : " + address + ":" + PORT_NUMBER);
+        Console.WriteLine("Waiting for connection...");
         listener.Start();
 
-        for (int i = 0; i < MAX_CONNECTION; i++)
+        for (int i = 1; i <= MAX_CONNECTION+1; i++)
         {
-            new Thread(DoWork).Start();
+            if (i <= MAX_CONNECTION)
+            {
+                new Thread(DoWork).Start();
+            }
+            else
+            {
+                new Thread(DoWork_Deny).Start();
+            }
         }
     }
-
+    static void DoWork_Deny()
+    {
+        while (true)
+        {
+            Socket soc = listener.AcceptSocket();
+            var stream = new NetworkStream(soc);
+            var reader = new StreamReader(stream);
+            var writer = new StreamWriter(stream);
+            writer.AutoFlush = true;
+            Console.WriteLine("Connection received from(DENIED): {0}",
+                                  soc.RemoteEndPoint);
+            writer.WriteLine("503 Service Unavalible, Close Connection !");
+            stream.Close();
+            //soc.Close();
+        }
+    }
     static void DoWork()
     {
         while (true)
         {
             Socket soc = listener.AcceptSocket();
 
-            Console.WriteLine("Da nhan duoc ket noi tu: {0}",
-                              soc.RemoteEndPoint);
 
-            try
+            Console.WriteLine("Connection received from: {0}",
+                              soc.RemoteEndPoint);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("IP:Port of Client: " + soc.RemoteEndPoint + ";" + "Connect At: " + DateTime.Now);
+            foreach (EndPoint EP in ConnectedClients)
             {
+                if (soc.RemoteEndPoint == EP)
+                {
+                    var stream = new NetworkStream(soc);
+                    var writer = new StreamWriter(stream);
+                    writer.AutoFlush = true;
+                    writer.WriteLine("429 Many Request !");
+                    sb.Append(" Disconect At : " + DateTime.Now + ";" + "Reason : 429 Many Request");
+                    File.AppendAllText("Access.log", sb.ToString());
+                    stream.Close();
+                    soc.Close();
+                }
+                else
+                {
+                    ConnectedClients.Add(soc.RemoteEndPoint);
+                }
+            }
+            try
+            {   
                 var stream = new NetworkStream(soc);
                 var reader = new StreamReader(stream);
                 var writer = new StreamWriter(stream);
                 writer.AutoFlush = true;
-                
+               
+
                 while (true)
                 {
                     string id = reader.ReadLine();
 
-                    if (String.IsNullOrEmpty(id))
+                    if (id.ToUpper() == "EXIT")
+                    {
+                        writer.WriteLine("BYE");
+                        sb.Append(";Disconnect At: " + DateTime.Now+";"+"Reason: Closed By Client\n");
+                        File.AppendAllText("access.log", sb.ToString());
+                        sb.Clear();
                         break; // disconnect
+                    }
+                        
 
                     if (_data.ContainsKey(id))
-                        writer.WriteLine("So ban da nhap la: '{0}'", _data[id]);
+                        writer.WriteLine("Number you've entered: '{0}'", _data[id]);
+                    else if (id.Contains("dig"))
+                    {
+                        string[] idcut = id.Split(' ');
+                        var ipaddress = Dns.GetHostAddresses(idcut[1])[0];
+                        writer.WriteLine("IP of Domain is : " + ipaddress);
+                    }
+                    else if (id.Contains("curl"))
+                    {
+                        string[] idcut = id.Split(' ');
+                        WebClient Client = new WebClient();
+                        Client.DownloadFile(idcut[1],"myfile.txt");
+                        writer.WriteLine("Content of Website has been downloaded !");
+                    }
                     else
-                        writer.WriteLine("So ban da nhap khong hop le !");
+                    {
+                        writer.WriteLine("Sai cu phap ! ");
+                    }
                 }
                 stream.Close();
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Loi: " + ex);
+                Console.WriteLine("Error: " + ex);
             }
 
-            Console.WriteLine("Da ngat ket noi : {0}",
+            Console.WriteLine("Client disconnected: {0}",
                               soc.RemoteEndPoint);
+            ConnectedClients.Remove(soc.RemoteEndPoint);
             soc.Close();
         }
     }
